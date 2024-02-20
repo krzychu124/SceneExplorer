10cac2ea11be4bc6efff64a5d64dfa66224e3492
krzychu124 <krzychu124@gmail.com>
2024-02-20 22:21:07 +0100

    Initial commit

---
+++ SceneExplorer/Logging.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using Colossal.Logging;
+
+namespace SceneExplorer
+{
+    public static class Logging
+    {
+        private static ILog _logger;
+
+        static Logging() {
+            try
+            {
+                _logger = LogManager.GetLogger("SceneExplorer").SetShowsErrorsInUI(false);
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogException(e);
+            }
+        }
+
+        public static void Info(string message) {
+            _logger.Info(message);
+        }
+        
+        [Conditional("DEBUG")]
+        public static void Debug(string message) {
+            _logger.Info(message);
+        }
+
+        public static void Warning(string message) {
+            _logger.Warn(message);
+        }
+
+        public static void Error(string message) {
+            _logger.Error(message);
+        }
+    }
+}
